bfeac626fd3efc6180bcbcc515824cc39e7db4ef
Jiří Šebele <sindriava@gmail.com>
2016-01-19 23:09:33 +0100

    Various improvements

--- init.el
+++ init.el
@@ -32,6 +32,26 @@
 ;; Don't indent with tabs
 (setq indent-tabs-mode nil)
 
+;; Various keybindings
+(global-set-key (kbd "C-x k") (lambda () (interactive) (kill-buffer (buffer-name))))
+(global-set-key (kbd "M-[") 'previous-buffer)
+(global-set-key (kbd "M-]") 'next-buffer)
+
+;; Better evaluation
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (end-of-defun)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
+(global-set-key (kbd "C-x C-e") 'eval-defun)
+(global-set-key (kbd "C-c C-e") 'eval-and-replace)
+
 ;; Package setup
 (require 'cl)
 (require 'package)
@@ -52,23 +72,70 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; Cleaner mode list
+(defvar mode-line-cleaner-alist
+  `(;(auto-complete-mode . " α")
+    ;(yas/minor-mode . " υ")
+    (paredit-mode . nil)
+    (company-mode . nil)
+    ;(eldoc-mode . "")
+    ;(abbrev-mode . "")
+    ;; Major modes
+    ;(lisp-interaction-mode . "λ")
+    ;(hi-lock-mode . "")
+    ;(python-mode . "Py")
+    ;(emacs-lisp-mode . "EL")
+    ;(nxhtml-mode . "nx")
+    (haskell-mode . ">>=")
+    ))
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for cleaner in mode-line-cleaner-alist
+        do (let* ((mode (car cleaner))
+                 (mode-str (cdr cleaner))
+                 (old-mode-str (cdr (assq mode minor-mode-alist))))
+             (when old-mode-str
+                 (setcar old-mode-str mode-str))
+               ;; major mode
+             (when (eq mode major-mode)
+               (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
+;; Disable all keybindings
+; (use-global-map (make-sparse-keymap))
+
 ;; Custom modal editing
-(defvar navigation-mode-map (make-sparse-keymap))
+;(defvar navigation-mode-map (make-sparse-keymap))
 
-(define-key navigation-mode-map (kbd "h") 'backward-char)
-(define-key navigation-mode-map (kbd "j") 'next-line)
-(define-key navigation-mode-map (kbd "k") 'previous-line)
-(define-key navigation-mode-map (kbd "l") 'forward-char)
+;(define-key navigation-mode-map (kbd "h") 'backward-char)
+;(define-key navigation-mode-map (kbd "j") 'next-line)
+;(define-key navigation-mode-map (kbd "k") 'previous-line)
+;(define-key navigation-mode-map (kbd "l") 'forward-char)
+;(define-key navigation-mode-map (kbd "M-x") 'execute-extended-command)
+;(define-key navigation-mode-map (kbd "i") 'insert-mode)
 
-(define-minor-mode navigation-mode
-  "Navigation mode" nil "<N>"
-  navigation-mode-map
-  (setq-local cursor-type
-              (if navigation-mode
-                  'box
-                  (default-value 'cursor-type))))
+;; (define-minor-mode navigation-mode
+;;   "Navigation mode" nil "<N>"
+;;   navigation-mode-map
+;;   (setq-local cursor-type
+;;               (if navigation-mode
+;;                   'box
+;; 		(default-value 'cursor-type))))
 
-(define-key global-map (kbd "<escape>") 'navigation-mode)
+;; (defvar insert-mode-map (make-sparse-keymap))
+
+;; (define-key insert-mode-map [t] #'self-insert-command)
+;; (define-key insert-mode-map (kbd "<escape>") 'navigation-mode)
+
+;; (define-minor-mode insert-mode
+;;   "Insert mode" nil "<I>"
+;;   insert-mode-map
+;;   (setq-local cursor-type
+;;               (if insert-mode
+;;                   'bar
+;;                 (default-value 'cursor-type))))
 
 ;; Magit
 (global-set-key (kbd "C-x g") 'magit-status)
@@ -99,9 +166,15 @@
                       :foreground (face-foreground 'default)
                       :background (face-background 'default))
 
-;; Line numbering
-(setq linum-format "%3d ")
-(global-linum-mode)
+;; Dired mode
+(add-hook 'dired-mode-hook (lambda ()
+			     (hl-line-mode)
+			     (setq-local cursor-type nil)))
+
+(with-eval-after-load "dired"
+  (define-key dired-mode-map (kbd "k") 'dired-previous-line)
+  (define-key dired-mode-map (kbd "j") 'dired-next-line)
+  (define-key dired-mode-map (kbd "J") 'dired-jump))
 
 ;; Backups
 (setq backup-directory-alist
