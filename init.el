34727160819064e676946b4b302323909c38b8ea
Jiří Šebele <sindriava@gmail.com>
2016-01-26 02:45:22 +0100

    Set up Eshell

--- init.el
+++ init.el
@@ -135,6 +135,46 @@
 		(:eval (when (fboundp 'vc-git-branches)
 			 (propertize (car (vc-git-branches)) 'face '(:foreground "#A6E22E"))))))
 
+;; Eshell
+(global-set-key (kbd "C-x C-e") 'eshell)
+
+(defun fish-path (path max-len)
+  "Return a potentially trimmed-down version of the directory PATH, replacing
+parent directories with their initial characters to try to get the character
+length of PATH (sans directory slashes) down to MAX-LEN."
+  (let* ((components (split-string (abbreviate-file-name path) "/"))
+         (len (+ (1- (length components))
+                 (cl-reduce '+ components :key 'length)))
+         (str ""))
+    (while (and (> len max-len)
+                (cdr components))
+      (setq str (concat str
+                        (cond ((= 0 (length (car components))) "/")
+                              ((= 1 (length (car components)))
+                               (concat (car components) "/"))
+                              (t
+                               (if (string= "."
+                                            (string (elt (car components) 0)))
+                                   (concat (substring (car components) 0 2)
+                                           "/")
+                                 (string (elt (car components) 0) ?/)))))
+            len (- len (1- (length (car components))))
+            components (cdr components)))
+    (concat str (cl-reduce (lambda (a b) (concat a "/" b)) components))))
+
+(add-hook 'eshell-mode-hook
+          (lambda ()
+	    (setq eshell-path-env (concat "/usr/local/bin:" eshell-path-env))
+	    (set-face-attribute 'eshell-prompt nil
+				:foreground 'unspecified
+				:weight 'unspecified)))
+
+(setq eshell-prompt-regexp "^λ [^ ]* ")
+(setq eshell-prompt-function (lambda ()
+                               (concat (propertize "λ " 'face '(:foreground "#F92672"))
+				       (fish-path (eshell/pwd) 1)
+				       " ")))
+
 ;; Magit
 (global-set-key (kbd "C-x g") 'magit-status)
 
