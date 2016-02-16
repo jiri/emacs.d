6596bcd72d75de0c7dde07d5665dbfd2e4b880d6
Jiří Šebele <sindriava@gmail.com>
2016-02-16 23:09:18 +0100

    Move Eshell config to `config/eshell-config.el'

--- init.el
+++ init.el
@@ -201,47 +201,7 @@
 ;; Eshell
 (global-set-key (kbd "C-c e") 'eshell)
 
-(defun fish-path (path max-len)
-  "Return a potentially trimmed-down version of the directory PATH, replacing
-parent directories with their initial characters to try to get the character
-length of PATH (sans directory slashes) down to MAX-LEN."
-  (let* ((components (split-string (abbreviate-file-name path) "/"))
-         (len (+ (1- (length components))
-                 (cl-reduce '+ components :key 'length)))
-         (str ""))
-    (while (and (> len max-len)
-                (cdr components))
-      (setq str (concat str
-                        (cond ((= 0 (length (car components))) "/")
-                              ((= 1 (length (car components)))
-                               (concat (car components) "/"))
-                              (t
-                               (if (string= "."
-                                            (string (elt (car components) 0)))
-                                   (concat (substring (car components) 0 2)
-                                           "/")
-                                 (string (elt (car components) 0) ?/)))))
-            len (- len (1- (length (car components))))
-            components (cdr components)))
-    (concat str (cl-reduce (lambda (a b) (concat a "/" b)) components))))
-
-(add-hook 'eshell-mode-hook
-          (lambda ()
-	    (define-key eshell-mode-map (kbd "C-c C-l")
-	      (lambda ()
-		(interactive)
-		(eshell/clear-scrollback)
-		(eshell-send-input nil nil t)))
-	    (setq eshell-path-env (concat "/usr/local/bin:" eshell-path-env))
-	    (set-face-attribute 'eshell-prompt nil
-				:foreground 'unspecified
-				:weight 'unspecified)))
-
-(setq eshell-prompt-regexp "^λ [^ ]* ")
-(setq eshell-prompt-function (lambda ()
-                               (concat (propertize "λ " 'face '(:foreground "#F92672"))
-				       (fish-path (eshell/pwd) 1)
-				       " ")))
+(eval-after-load 'eshell '(require 'eshell-config))
 
 ;; Which key
 (which-key-mode)
