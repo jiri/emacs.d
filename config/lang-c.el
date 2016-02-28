aef6441c7f794c2b0b9bb5842dddc68adbb0d354
Jiří Šebele <sindriava@gmail.com>
2016-02-28 23:15:08 +0100

    Fix C completion and improve it

--- config/lang-c.el
+++ config/lang-c.el
@@ -2,13 +2,15 @@
   :config
   (progn
     ;; Completion
-    (require 'semantic)
-    (global-semanticdb-minor-mode)
-    (global-semantic-idle-scheduler-mode)
-
-    (add-hook 'c-mode-common-hook 'semantic-mode)
-
     (with-eval-after-load 'company
+
+      (require 'semantic)
+      (global-semanticdb-minor-mode)
+      (global-semantic-idle-scheduler-mode)
+
+      (add-hook 'c-mode-common-hook 'semantic-mode)
+
+
       (defun sindriava/indent-or-complete ()
         "Indent if appropriate, start completion otherwise."
         (interactive)
@@ -16,16 +18,19 @@
             (company-complete-common)
           (indent-according-to-mode)))
 
-      (define-key c-mode-map (kbd "TAB") 'sindriava/indent-or-complete)
-      (define-key c++-mode-map (kbd "TAB") 'sindriava/indent-or-complete))
+      ;; (with-eval-after-load 'cc-mode
+      ;;   (define-key c-mode-map (kbd "TAB") 'sindriava/indent-or-complete)
+      ;;   (define-key c++-mode-map (kbd "TAB") 'sindriava/indent-or-complete))
+      )
 
     ;; Compilation
-    (defun sindriava/compile ()
-      (interactive)
-      (setq-local compilation-read-command nil)
-      (call-interactively 'compile))
+    (with-eval-after-load 'cc-mode
+      (defun sindriava/compile ()
+        (interactive)
+        (setq-local compilation-read-command nil)
+        (call-interactively 'compile))
 
-    (define-key c-mode-map (kbd "C-c C-c") 'sindriava/compile)))
+      (define-key c-mode-map (kbd "C-c C-c") 'sindriava/compile))))
 
 ;; Provide module
 (provide 'lang-c)
