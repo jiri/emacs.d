65b07e31085dd7c4841ef02159349eac897874d2
Jiří Šebele <sindriava@gmail.com>
2016-03-08 17:15:55 +0100

    Swap Meta and Control in Org mode

--- config/org-config.el
+++ config/org-config.el
@@ -3,8 +3,18 @@
 (setq org-src-window-setup 'current-window)
 
 ;; Improve keybindings
-(define-key org-mode-map (kbd "C-RET") 'org-insert-heading)
-(define-key org-mode-map (kbd "M-RET") 'org-insert-heading-respect-content)
+(define-key org-mode-map (kbd "C-<return>") 'org-insert-heading)
+(define-key org-mode-map (kbd "M-<return>") 'org-insert-heading-respect-content)
+
+(define-key org-mode-map (kbd "C-<left>") 'org-metaleft)
+(define-key org-mode-map (kbd "C-<right>") 'org-metaright)
+(define-key org-mode-map (kbd "C-S-<left>") 'org-shiftmetaleft)
+(define-key org-mode-map (kbd "C-S-<right>") 'org-shiftmetaright)
+
+(define-key org-mode-map (kbd "M-<left>") 'left-word)
+(define-key org-mode-map (kbd "M-<right>") 'right-word)
+(define-key org-mode-map (kbd "M-S-<left>") 'org-shiftcontrolleft)
+(define-key org-mode-map (kbd "M-S-<right>") 'org-shiftcontrolright)
 
 ;; Make clocks persistent
 (setq org-clock-persist 'history)
