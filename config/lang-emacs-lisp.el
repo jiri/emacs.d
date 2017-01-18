e090c6b822f9d8f6133cd8a2c9b0ae45dda37dfb
Jiří Šebele <sindriava@gmail.com>
2017-01-18 20:24:02 +0100

    Nuke everything

--- config/lang-emacs-lisp.el
+++
@@ -1,9 +0,0 @@
-;; Enable appropriate minor modes
-(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-
-;; Rename major modes
-(mode-line-clean 'lisp-interaction-mode "λ")
-(mode-line-clean 'emacs-lisp-mode "λ")
-
-;; Provide module
-(provide 'lang-emacs-lisp)
